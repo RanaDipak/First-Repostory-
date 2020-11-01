2dd68c5e1ad0296530aeff54cfdbb7ec1afe86fc
Dipak Rana <dipakrana2001@gmail.comgit config --global user.email dipakrana2001@gmail.com>
2020-11-01 17:07:52 +0530

    Inverse Number

---
+++ Java Program/Inverse.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Inverse {
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int inv=0;
+            int op=1;
+            while(n!=0){
+                int od=n%10;
+                int id=op;
+                int ip=od;
+                inv=inv+id*(int)Math.pow(10, ip-1);
+                n=n/10;
+                op++;
+            }
+            System.out.println(inv);
+        }
+}
+
