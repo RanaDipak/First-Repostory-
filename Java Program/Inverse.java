7e21627c77d2c29a4332ce3aff69e7bd78fc29b8
Dipak Rana <dipakrana2001@gmail.comgit config --global user.email dipakrana2001@gmail.com>
2020-11-01 17:11:24 +0530

    second method Inverse

--- Java Program/Inverse.java
+++ Java Program/Inverse.java
@@ -15,6 +15,23 @@ public class Inverse {
                 op++;
             }
             System.out.println(inv);
+
+
+                                         //second Method
+                                         
+            // Scanner scn = new Scanner(System.in);
+            // int n = scn.nextInt();
+    
+            // int i = 0;
+            // int p = 1;
+            // while(n > 0){
+            //     int dig = n % 10;
+            //     n = n / 10;
+            //     i += p * Math.pow(10, dig - 1);
+            //     p++;
+            // }
+    
+            // System.out.println(i);
         }
 }
 
