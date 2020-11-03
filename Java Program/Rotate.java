57f32f0a5b4623c16a11fe0e3a805b92706549a0
Dipak Rana <dipakrana2001@gmail.comgit config --global user.email dipakrana2001@gmail.com>
2020-11-03 17:49:19 +0530

    Rotate

---
+++ Java Program/Rotate.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Rotate {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int temp=n;
+        int node=0;
+        while(temp>0){
+            temp=temp/10;
+            node++;
+        }
+        k=k%node;
+        if(k<0){
+            k=k+node;
+        }
+        int div=1;
+        int mul=1;
+        for(int i=1;i<=node;i++){
+            if(i<=k){
+            div=div*10;
+            }else{
+                mul=mul*10;
+            }
+        }
+        int q=n/div;
+        int r=n%div;
+        int rot=r*mul+q;
+        System.out.println(rot);
+    }
+}
