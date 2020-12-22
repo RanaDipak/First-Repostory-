ebf04b67ad7d12c6ed7a98b5aae308cb5da9f872
Dipak Rana <dipakrana2001@gmail.comgit config --global user.email dipakrana2001@gmail.com>
2020-12-22 15:54:26 +0530

    chnage fibonacci

--- Java Program/Dynamic programing/Fibonacci.java
+++ Java Program/Dynamic programing/Fibonacci.java
@@ -4,16 +4,21 @@ public class Fibonacci{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int fibn = fib(n);
+        int fibn = fibMemoized(n, new int[n + 1]);
         System.out.println(fibn);
     }
-    public static int fib(int n ){
+    public static int fibMemoized(int n, int qb[]){
         if(n == 0 || n == 1){
-            return n;
+            return n; 
         }
-        int fibnm1 = fib(n - 1);
-        int fibnm2 = fib(n - 2);
+        if(qb[n] != 0){
+            return qb[n];
+        }
+        // System.out.println("hello" + n);
+        int fibnm1 = fibMemoized(n - 1, qb);
+        int fibnm2 = fibMemoized(n - 2, qb);
         int fibn = fibnm1 + fibnm2;
-        return  fibn;
+        qb[n] = fibn;
+        return fibn;
     }
 }
